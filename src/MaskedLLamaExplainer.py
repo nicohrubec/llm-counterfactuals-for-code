d4873020f83fc3a6aae4639f9a48211f4350cb74
Nicolas Hrubec <nicolas.hrubec@outlook.com>
2024-01-16 08:22:10 +0100

    Add MaskedLLamaExplainer

---
+++ src/MaskedLLamaExplainer.py
@@ -0,0 +1,53 @@
+import os
+import torch
+from transformers import pipeline, AutoTokenizer
+
+from helpers import build_masked_prompt, extract_code_from_string
+from Explainer import Explainer
+
+
+class MaskedLLamaExplainer(Explainer):
+    def __init__(self, model_str):
+        token = os.environ.get("LLAMA_KEY")
+        self.tokenizer = AutoTokenizer.from_pretrained(model_str, token=token)
+        self.llm = pipeline(
+            "text-generation",
+            model=model_str,
+            torch_dtype=torch.bfloat16,
+            device="cuda:0",
+            token=token,
+            tokenizer=self.tokenizer
+        )
+
+    def build_prompt(self, prompt):
+        prompt = f"""
+<s>[INST] <<SYS>>
+{self.system_prompt}
+<</SYS>>
+
+{prompt} [/INST] 
+"""
+        prompt_len = len(prompt)
+        prompt += " <code>"
+
+        return prompt, prompt_len
+
+    def explain(self, sample: str, prediction: bool) -> str:
+        prompt = build_masked_prompt(sample, prediction)
+        prompt, prompt_len = self.build_prompt(prompt)
+
+        sequences = self.llm(
+            prompt,
+            do_sample=True,
+            top_k=10,
+            num_return_sequences=1,
+            eos_token_id=self.tokenizer.eos_token_id,
+            max_length=3000,
+        )
+
+        response = sequences[0]['generated_text']
+        explanation = extract_code_from_string(response[prompt_len:])
+
+        print(explanation)
+
+        return explanation
