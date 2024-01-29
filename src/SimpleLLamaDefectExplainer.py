b1f8b6fd00fbf6a62cc0474c00aa39cb065cb2cc
Nicolas Hrubec <nicolas.hrubec@outlook.com>
2024-01-29 11:21:49 +0100

    Revert "Fix: Unwrap sample list in explainer"
    This reverts commit 66fab683d6489cbdc3c2b5e76b50ee7218d71394.

--- src/SimpleLLamaDefectExplainer.py
+++ src/SimpleLLamaDefectExplainer.py
@@ -5,7 +5,7 @@ from LLamaExplainer import LLamaExplainer
 
 class SimpleLLamaDefectExplainer(LLamaExplainer):
     def explain(self, sample, prediction: bool) -> str:
-        prompt = build_explainer_prompt(sample[0], prediction)
+        prompt = build_explainer_prompt(sample, prediction)
         prompt, prompt_len = build_llama_prompt(self.system_prompt, prompt)
         response = self.ask_llama(prompt)
         explanation = extract_code_from_string(response[prompt_len:])
