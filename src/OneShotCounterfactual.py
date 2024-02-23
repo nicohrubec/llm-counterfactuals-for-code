3238dfc4b0e2656fbdb9c99a9839779c95caea00
Nicolas Hrubec <nicolas.hrubec@outlook.com>
2024-02-23 17:37:15 +0100

    Update Simple Prompting CounterfactualGenerator to handle multiple explanations

--- src/OneShotCounterfactual.py
+++ src/OneShotCounterfactual.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 from Levenshtein import distance
+import heapq
 
 from CounterfactualGenerator import CounterfactualGenerator
 from WrongPredictionError import WrongPredictionError
@@ -12,10 +13,30 @@ class OneShotCounterfactual(CounterfactualGenerator):
         if original_label != target:
             raise WrongPredictionError
 
-        candidate_counterfactual = self.explainer.explain(sample, original_label)[0]
-        counterfactual_label, counterfactual_score = self.blackbox(candidate_counterfactual)
-        similarity_score = float(self.similarity_score(sample, candidate_counterfactual)[0][0])
-        token_distance = distance(sample, candidate_counterfactual)
+        candidate_counterfactuals = self.explainer.explain(sample, original_label)
+        counterfactual_scores = []
+        heapq.heapify(counterfactual_scores)
+        counterfactual_found = False
+
+        for candidate_counterfactual in candidate_counterfactuals:
+            counterfactual_label, counterfactual_score = self.blackbox(candidate_counterfactual)
+            similarity_score = float(self.similarity_score(sample, candidate_counterfactual)[0][0])
+            token_distance = distance(sample, candidate_counterfactual)
+
+            if counterfactual_label != original_label:
+                counterfactual_found = True
+
+            heapq.heappush(counterfactual_scores, (token_distance, counterfactual_label, counterfactual_score,
+                                                   candidate_counterfactual, similarity_score))
+
+        counterfactual_label = original_label
+        if counterfactual_found:  # find counterfactual with minimal token distance to original sample
+            while counterfactual_label == original_label:
+                token_distance, counterfactual_label, counterfactual_score, candidate_counterfactual, \
+                    similarity_score = heapq.heappop(counterfactual_scores)
+        else:  # no counterfactual found, return minimal token distance sample
+            token_distance, counterfactual_label, counterfactual_score, candidate_counterfactual, similarity_score, = \
+                heapq.heappop(counterfactual_scores)
 
         print(f"The correct label is: {target}")
         print(f"Originally the model predicted {original_label} with a confidence of {original_score}.")
